01e88f9b1767d3db8b2dcb17dcebd38fbb448910
15056215 <15056215@myrp.edu.sg>
2017-05-19 14:53:42 +0800

    updated

--- app/src/main/java/com/example/a126308/p05_ndpsongs/DBHelper.java
+++ app/src/main/java/com/example/a126308/p05_ndpsongs/DBHelper.java
@@ -69,4 +69,6 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
 
+
+
 }
---
+++ app/src/main/java/com/example/a126308/p05_ndpsongs/ModifySong.java
@@ -0,0 +1,20 @@
+package com.example.a126308.p05_ndpsongs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 15056215 on 19/5/2017.
+ */
+
+public class ModifySong extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_song);
+
+    }
+
+}
