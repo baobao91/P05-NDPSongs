aa1e2684669bb368719c9565df3f36f901248410
15056215 <15056215@myrp.edu.sg>
2017-05-19 15:48:46 +0800

    updated

--- app/src/main/java/com/example/a126308/p05_ndpsongs/DBHelper.java
+++ app/src/main/java/com/example/a126308/p05_ndpsongs/DBHelper.java
@@ -67,7 +67,7 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     //To perform UPDATE
-    public int updateNote(Song data){
+    public int updateSong(Song data){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
 
@@ -85,6 +85,18 @@ public class DBHelper extends SQLiteOpenHelper {
         db.close();
         return result;
     }
+
+    //To perform DELETE
+    public int deleteSong(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String condition = COLUMN_ID + "= ?";
+        String[] args = {String.valueOf(id)};
+        int result = db.delete(TABLE_SONG, condition, args);
+        db.close();
+        return result;
+    }
+
+
 
 
 
